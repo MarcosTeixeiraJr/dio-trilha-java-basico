9ada2741e735919e4232a035d25a4d8bf8a3c91c
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-05-13 14:43:17 -0300

    Iphone

--- iPhone/src/celular/aparelho/AparelhoCelular.java
+++ iPhone/src/celular/aparelho/AparelhoCelular.java
@@ -1,16 +1,16 @@
 package celular.aparelho;
 
 import celular.Iphone.IphoneFuncoes;
-import celular.internet.NavegadorInternet;
-import celular.ligacao.AparelhoTelefonico;
-import celular.musica.ReprodutorMusica;
 
 public class AparelhoCelular {
     public static void main(String[] args) {
+       // Criando um objeto da classe IphoneFuncoes
        IphoneFuncoes iphone = new IphoneFuncoes();
 
-       Iphone.iphone();
-       
-       
+       // Chamando os métodos da classe IphoneFuncoes
+       iphone.navegar(); // Chamando o método navegar()
+       iphone.ligar();   // Chamando o método ligar()
+       iphone.musica();  // Chamando o método musica()
     }
 }
+
