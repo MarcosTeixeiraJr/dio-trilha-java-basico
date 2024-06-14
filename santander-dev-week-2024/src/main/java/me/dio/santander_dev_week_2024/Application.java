332a11ddfd0b5d7f97fcd0689a77fa8181d5d5f0
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-06-14 12:43:23 -0300

    Java RESTful API WEEK
    Signed-off-by: Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>

---
+++ santander-dev-week-2024/src/main/java/me/dio/santander_dev_week_2024/Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_week_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ santander-dev-week-2024/src/test/java/me/dio/santander_dev_week_2024/SantanderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
